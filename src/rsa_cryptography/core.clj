07e93b922985d1bf2c29f88d92045649b34673bc
Topher <topher200@gmail.com>
2010-11-29 23:55:57 -0500

    initial implementation of extended-gcd

--- src/rsa_cryptography/core.clj
+++ src/rsa_cryptography/core.clj
@@ -1,1 +1,12 @@
 (ns rsa-cryptography.core)
+
+(defn extended-gcd
+  "Returns the [x y] for the extended Euclidean algorithm. Found here:
+  http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm"
+  [a b]
+  (if (= (mod a b) 0)
+    '(0 1)
+    (let [[x y] (extended-gcd (b (mod a b)))]
+      '(y (- x (* y (/ a b)))))))
+
+(println (extended-gcd 3120 17))
