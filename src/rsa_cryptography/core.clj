6b5acf88353d85dec53b379507443a232a59e4be
Topher Brown <topher200@gmail.com>
2011-04-02 02:29:36 -0400

    wrote a function to determine the modular multiplicative inverse
    uses the extended gcd algo. includes tests for it and helper functions

--- src/rsa_cryptography/core.clj
+++ src/rsa_cryptography/core.clj
@@ -1,10 +1,23 @@
-(ns rsa-cryptography.core)
+(ns rsa-cryptography.core
+  (:use [clojure.contrib.math]))
+
+(defn division-with-remainder [x y]
+  [(floor (/ x y)) (mod x y)])
 
 (defn extended-gcd
   "Returns the [x y] for the extended Euclidean algorithm. Found here:
   http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm"
   [a b]
-  (if (= (mod a b) 0)
-    [0 1]
-    (let [[x y] (extended-gcd b (mod a b))]
-      [y (- x (* y (/ a b)))])))
+  (if (= b 0)
+    [1 0]
+    (let [[q r] (division-with-remainder a b)
+          [s t] (extended-gcd b r)]
+      [t (- s (* q t))])))
+
+(defn modular-multiplicative-inverse
+  "Determines the inverse cooresponding to a given a with respect to modules m.
+  Research: http://en.wikipedia.org/wiki/Modular_multiplicative_inverse and
+  http://numericalrecipes.blogspot.com/2009/03/modular-multiplicative-inverse.html"
+  [a m]
+  (let [[x y] (extended-gcd a m)]
+    (mod x m)))
--- test/rsa_cryptography/test/core.clj
+++ test/rsa_cryptography/test/core.clj
@@ -2,5 +2,19 @@
   (:use [rsa-cryptography.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest division-with-remainder-test
+  (is (= [0 0] (division-with-remainder 0 1)))
+  (is (= [1 0] (division-with-remainder 1 1)))
+  (is (= [0 1] (division-with-remainder 1 2)))
+  (is (= [0 1] (division-with-remainder 1 3)))
+  (is (= [0 2] (division-with-remainder 2 3)))
+  (is (= [1 1] (division-with-remainder 3 2)))
+  (is (= [2 2] (division-with-remainder 8 3))))
+
+(deftest extended-gcd-test
+  (is (= [1 0] (extended-gcd 1 0)))
+  (is (= [-9 47] (extended-gcd 120 23)))
+  (is (= [-367 2] (extended-gcd 17 3120))))
+
+(deftest modular-multiplicative-inverse-test
+  (is (= 2753 (modular-multiplicative-inverse 17 3120))))
