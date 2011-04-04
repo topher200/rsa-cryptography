6048af215220f5680d30ab824fa9b81d4e2bbf35
Topher Brown <topher200@gmail.com>
2011-04-03 20:58:22 -0400

    wrote a test for check-solution-keys
    uses new (def test-keys), along with generate-keys-list

--- test/rsa_cryptography/test/attack.clj
+++ test/rsa_cryptography/test/attack.clj
@@ -2,9 +2,14 @@
   (:use [rsa-cryptography.attack] :reload)
   (:use [clojure.test]))
 
+(def test-keys {:d 399707681, :e 65537, :modulus 3306810623})
+
 (deftest generate-keys-list-test
-  (let [keys {:d 399707681, :e 65537, :modulus 3306810623}]
-    (is (= (generate-keys-list keys 100 3)
-           '({:d 100, :e 65537, :modulus 3306810623}
-             {:d 101, :e 65537, :modulus 3306810623}
-             {:d 102, :e 65537, :modulus 3306810623})))))
+  (is (= (generate-keys-list test-keys 100 3)
+         '({:d 100, :e 65537, :modulus 3306810623}
+           {:d 101, :e 65537, :modulus 3306810623}
+           {:d 102, :e 65537, :modulus 3306810623}))))
+
+(deftest check-solution-keys-test
+  (is (check-solution-keys (generate-keys-list test-keys)))
+  (is (not (check-solution-keys (generate-keys-list test-keys 100 1)))))
